89d1b49eca3ca209426c840d09dad78502206087
GreemerBR <gregory.v.zimmer@gmail.com>
2022-05-06 16:53:26 -0300

    Continuação do Desafio 03

--- WarrenTechAcademy.Desafios/Desafio03.cs
+++ WarrenTechAcademy.Desafios/Desafio03.cs
@@ -10,7 +10,24 @@ namespace WarrenTechAcademy.Desafios
     {
         public void Executar()
         {
+            Console.Clear();
+            Console.Write("Informe o número desejado: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            List<int> valor = new List<int>();
+            int indice = 0;
 
+            Console.Write("\nInsira os números pertencentes ao vetor. Estes devem ser positivos." +
+          "\nCaso queira parar de inserir números digite um número negativo." +
+          "\n");
+            valor.Add(Convert.ToInt32(Console.ReadLine()));
+
+            while (valor[indice] >= 0)
+            {
+                valor.Add(Convert.ToInt32(Console.ReadLine()));
+                indice++;
+            }
+
+            
         }
     }
 }
